c0f313b544f9fb579e6c3ef5d8f640f765d5894e
jhonatanmizu <natanjesuss20@gmail.com>
2025-02-24 11:56:43 -0300

    chore: setup pre-commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+def is_natural():
+    """This function prints hello and bye messages."""
+    print("Hello")
+    print("Bye")
+
+
+def get_user_input():
+    """This function gets user input."""
+    user_input = input("Enter something: ")
+    return user_input
+
+
+def show_user_input(user_input):
+    """This function shows user input."""
+    print("You entered: " + user_input)
+
+
+def random_logic():
+    """This function does some random logic."""
+    print("Random logic")
+
+
+def say_hadouken():
+    """This function return a hadouken string"""
+    return "hadouken"
