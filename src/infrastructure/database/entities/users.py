e1fd638c5a3e1d4452e8c9d5e113adcbfaf481c0
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-21 00:32:11 -0300

    chore: update folder name

--- src/infrastructure/database/entities/users.py
+++ src/infrastructure/database/entities/users.py
@@ -2,7 +2,7 @@ from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import DateTime
 
-from src.infraestructure.database.settings.base import Base
+from src.infrastructure.database.settings.base import Base
 
 
 class Users(Base):
