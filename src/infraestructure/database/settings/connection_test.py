b57459b12cf6131efdd4fa0c82bcf059100e4106
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-21 00:30:52 -0300

    test: create user

--- src/infraestructure/database/settings/connection_test.py
+++ src/infraestructure/database/settings/connection_test.py
@@ -1,16 +1,29 @@
+from sqlalchemy import text
+
 from .connection import DBConnectionHandler
 
 
 def test_connection():
-    """Test connection"""
+    """ Test connection """
     db_connection = DBConnectionHandler()
     engine = db_connection.get_engine()
     assert engine is not None
 
 
 def test_create_database_engine():
-    """Test create database engine"""
+    """ Test create database engine """
     db_connection = DBConnectionHandler()
     engine = db_connection.get_engine()
     assert engine is not None
     assert engine.name == "mysql"
+
+
+def test_create_user():
+    """ Test create user """
+    db_connection = DBConnectionHandler()
+    engine = db_connection.get_engine()
+    connection = engine.connect()
+    connection.execute(
+        text("INSERT INTO users (first_name,last_name) values ('Hi','m1zu')")
+    )
+    connection.commit()
