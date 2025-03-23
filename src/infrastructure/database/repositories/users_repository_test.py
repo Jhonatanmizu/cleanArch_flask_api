6993445130d2a5350dbe583eb53b0e5e736d1a5d
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-22 23:24:40 -0300

    test: find user by id and find user by first_name

--- src/infrastructure/database/repositories/users_repository_test.py
+++ src/infrastructure/database/repositories/users_repository_test.py
@@ -35,3 +35,51 @@ def test_insert_user():
         DELETE FROM users WHERE id = {registry.id}
     '''))
     connection.commit()
+
+
+def test_find_user_by_first_name():
+    """_summary_
+        test find the user by first_name
+    """
+    mocked_first_name = "John"
+    mocked_last_name = "Doe"
+    mocked_birthdate = datetime(year=2000, month=11, day=24)
+    user_repo = UserRepository()
+    user_repo.insert_user(first_name=mocked_first_name,
+                          last_name=mocked_last_name,
+                          birthdate=mocked_birthdate)
+
+    registry = user_repo.find_user_by_first_name(first_name=mocked_first_name)
+
+    assert registry is not None
+    assert registry.first_name == mocked_first_name
+    connection.execute(text(f'''
+        DELETE FROM users WHERE id = {registry.id}
+    '''))
+    connection.commit()
+
+
+def test_find_user_by_id():
+    """_summary_
+        test find the user by id
+    """
+    mocked_id = 9999
+    sql = text("""
+            INSERT INTO users (id, first_name, last_name, birthdate) 
+            VALUES (:id, :first_name, :last_name, :birthdate)
+        """)
+    connection.execute(
+        sql,
+        {"id": mocked_id, "first_name": "Hi",
+            "last_name": "Mizu", "birthdate": datetime.now()}
+    )
+    connection.commit()
+    user_repo = UserRepository()
+    registry = user_repo.find_user_by_id(_id=mocked_id)
+
+    assert registry is not None
+    assert registry.id == mocked_id
+    connection.execute(text(f'''
+        DELETE FROM users WHERE id = {registry.id}
+    '''))
+    connection.commit()
