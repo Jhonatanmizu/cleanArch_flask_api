515fc7eb0d15328d5922bde15d9ed823948fab11
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-22 23:24:05 -0300

    feat: implement find user by id and by first_name

--- src/infrastructure/database/repositories/users_repository.py
+++ src/infrastructure/database/repositories/users_repository.py
@@ -1,6 +1,8 @@
 
 from datetime import datetime
 
+from sqlalchemy import text
+
 from src.infrastructure.database.entities.users import Users as UsersEntity
 from src.infrastructure.database.settings.connection import DBConnectionHandler
 
@@ -33,6 +35,59 @@ class UserRepository:
                 )
                 database.session.add(new_user)
                 database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+
+    @classmethod
+    def find_user_by_id(cls, _id: int) -> UsersEntity | None:
+        """_summary_
+
+            Find user by id
+        Args:
+            _id (int): _description_
+
+        Raises:
+            exception: _description_
+
+        Returns:
+            UsersEntity | None: _description_
+        """
+        with DBConnectionHandler() as database:
+            if not database.session:
+                return None
+            try:
+
+                user = database.session.query(
+                    UsersEntity).filter(UsersEntity.id == _id).first()
+                return user
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+
+    @classmethod
+    def find_user_by_first_name(cls, first_name: str) -> UsersEntity | None:
+        """_summary_
+            Find user by first_name and last_name
+        Args:
+            first_name (str): _description_
+
+        Raises:
+            exception: _description_
+
+        Returns:
+            UsersEntity | None: _description_
+        """
+        with DBConnectionHandler() as database:
+            if not database.session:
+                return None
+            try:
+
+                user_query = database.session.query(
+                    UsersEntity)
+                user = user_query.filter(
+                    UsersEntity.first_name == first_name).first()
+                return user
             except Exception as exception:
                 database.session.rollback()
                 raise exception
