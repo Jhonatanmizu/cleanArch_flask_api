fa1db65d085e61306d5c6ba03e1b58a0d26cbb34
jhonatanmizu <natanjesuss20@gmail.com>
2025-05-16 22:54:45 -0300

    test: invalid name and name too big

--- src/data/use_cases/find_user_test.py
+++ src/data/use_cases/find_user_test.py
@@ -1,4 +1,3 @@
-
 from src.data.use_cases.find_user import FindUser
 from src.infrastructure.database.tests.users_repository import \
     UsersRepositorySpy
@@ -24,3 +23,23 @@ def test_find_user_by_id():
     assert response['type'] == "Users"
     assert response['count'] == 1
     assert response["attributes"] != []
+
+def test_find_error_invalid_name():
+    first_name = "Invalid name123"
+    repository = UsersRepositorySpy()
+    find_user = FindUser(repository) # type: ignore
+    try:
+        find_user.find_by_first_name(first_name)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Name is invalid"
+
+def test_find_error_name_too_big():
+    name_too_large = "nameistobigtohandleinthiscase"
+    respository = UsersRepositorySpy()
+    find_user = FindUser(respository) # type: ignore
+    try:
+        find_user.find_by_first_name(name_too_large)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Name is too big"
